69de461fe813b72be5ae9ce26871cc06fe5d6253
sadik <sadikmd2k01@gmail.com>
2025-02-12 16:29:05 +0530

    .

--- simple_programs/Quadratic_equation.py
+++ simple_programs/Quadratic_equation.py
@@ -3,6 +3,7 @@ def quadratic_eqaution(a,b,c):
     equ_2= (-b - (b**2 - 4*a*c)**0.5)/2*a
     return f"The roots for the given Quadratic equation is {equ_1} and {equ_2}"
 
+
 a= float(input("Enter coefficient a: " ))
 b= float(input("Enter coefficient a: " ))
 c= float(input("Enter coefficient a: " ))
