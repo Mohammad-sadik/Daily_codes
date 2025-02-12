c47e433a377335eb62b92bdb82c762599e9286a3
sadik <sadikmd2k01@gmail.com>
2025-02-12 16:29:12 +0530

    .

--- simple_programs/Swap_value_of_two_variables.py
+++ simple_programs/Swap_value_of_two_variables.py
@@ -1,6 +1,7 @@
 a=int(input("enter A value : "))
 b=int(input("enter B value : "))
 print(f"The value of A is {a} & The value of B is {b} before swaping.")
+"""__________________________________________________________________________"""
 # a=b^a
 # b=a^b
 # a=a^b
