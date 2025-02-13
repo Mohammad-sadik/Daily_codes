af186dfefb293a3002c31e8fd0922519f4fb5070
sadik <sadikmd2k01@gmail.com>
2025-02-13 10:09:50 +0530

    .

--- loops/Square_n.py
+++ loops/Square_n.py
@@ -1,4 +1,5 @@
 # You need to find the sqaure of Nth number
 n =int(input("Enter a number : "))
 print(f"The sqaure of {n} is {n**2}")
-
+# --------------------------------------------
+print(f"The sqaure of {n} is {n*n}")
