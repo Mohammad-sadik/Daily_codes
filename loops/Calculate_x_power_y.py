f0a97e5d01cf0bd1ffada5c4bacdcbfb480ffb22
sadik <sadikmd2k01@gmail.com>
2025-02-13 10:08:57 +0530

    .

--- loops/Calculate_x_power_y.py
+++ loops/Calculate_x_power_y.py
@@ -2,3 +2,8 @@
 x = int(input("Enter x value : "))
 y= int(input("Enter y value : "))
 print(f"The {x} to the power of {y} is equal to {x**y}")
+#-----------------------------------------------------------
+sqr=1
+for i in range(y):
+    sqr=x* sqr
+print(f"The {x} to the power of {y} is equal to {sqr}")
