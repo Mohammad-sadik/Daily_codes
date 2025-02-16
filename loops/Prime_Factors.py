53ded45931cb9480ac26eb75fa5f50f48c1cc8e1
sadik <sadikmd2k01@gmail.com>
2025-02-16 17:21:48 +0530

    Prime Factors.

--- loops/Prime_Factors.py
+++ loops/Prime_Factors.py
@@ -1,13 +1,15 @@
+def prime_factor(num):
+    i = 2
+    List_a = []
+    list_b = []
+    while num != 1:
+        if num % i == 0:
+            List_a.append(i)
+        for j in List_a[::]:
+            if num % j == 0:
+                list_b.append(j)
+                num = num / j
+        i += 1
+    return list_b
 num=int(input("Enter a number : "))
-i=2
-List_a=[]
-list_b=[]
-while num!=1:
-    if num%i==0:
-        List_a.append(i)
-    for j in List_a[::]:
-        if num%j==0:
-            list_b.append(j)
-            num= num/j
-    i+=1
-print(list_b)
+print(prime_factor(num))
