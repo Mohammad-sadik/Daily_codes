66107fc1c091e6fef6177b58ead130f6f696d183
sadik <sadikmd2k01@gmail.com>
2025-02-16 17:34:05 +0530

    Bin to dec and oct.

---
+++ loops/Bin_to_dec_and_oct.py
@@ -0,0 +1,3 @@
+bin_num=input("Enter Bin value here : ")
+print(f"The binary num is {bin_num}, after converting to the Decimal is {int(bin_num, 2)}")
+print(f"The binary num is {bin_num}, after converting to the Octal is {oct(int(bin_num,2))[2:]}")
