109dc88b50e351b0aa88400ec53f7c59cf7bd72e
sadik <sadikmd2k01@gmail.com>
2025-02-17 16:34:38 +0530

    Program to Check Armstrong Number.

---
+++ loops/amstrong_checking.py
@@ -0,0 +1,15 @@
+def Amstrong_checking(num):
+    orginal = num
+    l = len(str(num))
+    total = 0
+    for i in range(l):
+        digit = num % 10
+        total = total + digit ** l
+        num = num // 10
+        print(f"{i}: digit :{digit}, total:{total}")
+    if orginal == total:
+        return f"{orginal} is an Armstrong number."
+    else:
+        return f"{orginal} is an Not Armstrong number."
+num = int(input("Enter number : "))
+print(Amstrong_checking(num))
