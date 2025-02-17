d3f58e928c7f6d8eaaa2f9c6562097a53947fe5f
sadik <sadikmd2k01@gmail.com>
2025-02-17 16:06:13 +0530

    Reverse the digits of given number.

--- loops/Reverse_number.py
+++ loops/Reverse_number.py
@@ -1,5 +1,12 @@
-num=input("Enter your number : ")
-fn=""
-for i in num[-1::-1]:
-    fn+=i
-print(f" The given number is {num} & Reverse the digits of is {fn}")
+def is_palindrome_number(num):
+    original=num
+    reversed_num=0
+    while num > 0:
+        digit = num % 10
+        reversed_num = reversed_num * 10 + digit
+        num = num // 10
+    return f"The given number {original} The reverse of the given num is {reversed_num}"
+
+num=int(input("Enter your number : "))
+num_f=0
+print(is_palindrome_number(num))
