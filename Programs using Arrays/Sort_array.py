97fc9a35646c98d52b8d7377f68f5ffc146b038b
sadik <sadikmd2k01@gmail.com>
2025-02-21 21:54:52 +0530

    Sort an array(quick_sort).

--- Programs using Arrays/Sort_array.py
+++ Programs using Arrays/Sort_array.py
@@ -1,7 +1,21 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr)//2]
+    left = [x for x in arr if x < pivot]
+    print(f"left :{left}")
+    middle = [x for x in arr if x == pivot]
+    print(f"middle :{middle}")
+    right = [x for x in arr if x > pivot]
+    print(f"right:{right}")
+    return quick_sort(left) + middle + quick_sort(right)
+
 arr = input("Enter array: ").split()
 arr = [int(i) for i in arr]
 # arr.sort()
 # print(arr)
 """_________"""
-arr=sorted(arr)
-print(arr)
+# arr=sorted(arr)
+# print(arr)
+"""_________"""
+print(quick_sort(arr))
