c355b4eb52e78b984748412775808b5fefa8f2e4
sadik <sadikmd2k01@gmail.com>
2025-02-23 16:53:24 +0530

    Power using recursion.

--- Programs using Recursion/Power_using_recursion.py
+++
@@ -1,7 +0,0 @@
-def power_using_recursion(num, power):
-    if power == 0:
-        return 1
-    return num * power_using_recursion(num, power - 1)
-num = int(input("Enter number: "))
-power = int(input("Enter power: "))
-print(f"The given number& power are {num} & {power}, and the of the number {power_using_recursion(num, power)}.")
