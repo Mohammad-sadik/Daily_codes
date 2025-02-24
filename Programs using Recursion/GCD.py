0952d5a14add911edf98498dfb87da60a7921249
sadik <sadikmd2k01@gmail.com>
2025-02-24 20:46:38 +0530

    GCD using recursion.

--- Programs using Recursion/GCD.py
+++ Programs using Recursion/GCD.py
@@ -1,8 +1,18 @@
-def gcd(a, b, max_v):
-    if (a % max_v == 0) and (b % max_v == 0):
-        return max_v
-    return gcd(a, b, max_v-1)
-fst_value = int(input("Enter first value : "))
-sec_value = int(input("Enter second value : "))
-max_v = (max(fst_value, sec_value)) // 2
-print(gcd(fst_value,sec_value, max_v))
+# def gcd(a, b, max_v):
+#     if (a % max_v == 0) and (b % max_v == 0):
+#         return max_v
+#     return gcd(a, b, max_v-1)
+# fst_value = int(input("Enter first value : "))
+# sec_value = int(input("Enter second value : "))
+# max_v = (max(fst_value, sec_value)) // 2
+# print(gcd(fst_value,sec_value, max_v))
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+# Example usage:
+fst_value = int(input("Enter first value: "))
+sec_value = int(input("Enter second value: "))
+
+print("GCD:", gcd(fst_value, sec_value))
