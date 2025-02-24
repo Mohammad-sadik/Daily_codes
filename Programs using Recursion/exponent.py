7ee4c6eb1496ca6cafc4c36f7df9e28ae058f0f9
sadik <sadikmd2k01@gmail.com>
2025-02-24 20:14:29 +0530

    Exponent using recursion.

--- Programs using Recursion/exponent.py
+++ Programs using Recursion/exponent.py
@@ -1,1 +1,9 @@
 
+def exponent_using_recursion(base, exponent):
+    if exponent == 0:
+        return 1
+    return base * exponent_using_recursion(base, exponent-1)
+
+base = int(input("Enter a Base: "))
+exponent = int(input("Enter the exponent: "))
+print(f"The base is {base} and exponent is {exponent} is equal to {exponent_using_recursion(base, exponent)}")
