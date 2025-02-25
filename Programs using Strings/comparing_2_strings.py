8ff837df94d85f4eea5b387a7535ba84959f2771
sadik <sadikmd2k01@gmail.com>
2025-02-25 20:00:46 +0530

    comparing 2 strings without any function.

--- Programs using Strings/comparing_2_strings.py
+++ Programs using Strings/comparing_2_strings.py
@@ -1,11 +1,11 @@
 def compare(str1, str2):
     if len(str1) != len(str2):
-        return "Strings are not equal"
+        return "Strings are not equal."
 
     for i in range(len(str2)):
         if str1[i] != str2[i]:
-            return "Strings are not equal"
-    return "Strings are equal"
+            return "Strings are not equal."
+    return "Strings are equal."
 
 str_1 = input("Enter first string: ")
 str_2 = input("Enter second string: ")
