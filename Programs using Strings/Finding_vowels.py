5374dfac71d577a7fafeb3ab5136eb1351fdd203
sadik <sadikmd2k01@gmail.com>
2025-02-27 20:44:01 +0530

    Finding vowels.

---
+++ Programs using Strings/Finding_vowels.py
@@ -0,0 +1,8 @@
+
+word = input("Enter word: ")
+vowels = ["a","e","i","o","u"]
+new_word = ""
+for i in word:
+    if i.lower() in vowels:
+        new_word+=i + " "
+print(f"The word is {word}, vowels in word : {new_word}")
