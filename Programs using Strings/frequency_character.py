7ad79d6dd337a60713966d3ed4d4acfae94e8f45
sadik <sadikmd2k01@gmail.com>
2025-02-28 18:17:26 +0530

    Find the frequency of a character in a string

--- Programs using Strings/frequency_character.py
+++ Programs using Strings/frequency_character.py
@@ -1,9 +1,9 @@
 string = input("Enter String: ")
 char = input("Enter Character: ")
-# count = 0
-# for chr_ in string:
-#     if chr_ == char:
-#         count += 1
-# print(f"Frequency of '{char}': {count}")
+count = 0
+for chr_ in string:
+    if chr_ == char:
+        count += 1
+print(f"Frequency of '{char}': {count}")
 
-print(f"Frequency of '{char}': {string.count(char)}")
+#print(f"Frequency of '{char}': {string.count(char)}")
