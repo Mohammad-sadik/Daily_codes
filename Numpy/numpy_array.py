a4963ef85001914d0537449a8091f8c873c453bc
sadik <sadikmd2k01@gmail.com>
2025-03-04 12:06:47 +0530

    1d, 2d, 3d array's

---
+++ Numpy/numpy_array.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr_1 = np.array([1,2,3]) #1D array (Vector)
+arr_2 = np.array([[1,2,3],[4,5,6]]) #a 2D Array (Matrix)
+arr_3 = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]]) #a 3D Array (Tensor)
+print(f"This is 1'D array: \n {arr_1}")
+
+print(f"This is 2'D array:\n  {arr_2}")
+
+print(f"This is 3'D array: \n {arr_3}")
