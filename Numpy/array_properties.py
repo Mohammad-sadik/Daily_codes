01bcf3659289adddf1721a78fd1a22595e1a9e15
sadik <sadikmd2k01@gmail.com>
2025-03-07 16:26:59 +0530

    Checking Array Properties.

---
+++ Numpy/array_properties.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(f"The shape of the array: {arr.shape}") # Shape of the array
+# Data type
+print(f"The data type of Array: {arr.dtype}")
+# Number of dimensions
+print(f"The Number of dimensions of Array:{arr.ndim}")
