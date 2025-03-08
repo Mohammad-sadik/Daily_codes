41d4900f0e28bf4c6e4cb83bb5c674b45eafb91a
sadik <sadikmd2k01@gmail.com>
2025-03-08 22:40:40 +0530

    .

---
+++ Numpy/Indexing_Slicing.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr = np.array([[10, 20, 30], [40, 50, 60]])
+print(arr[1,2]) #Indexing o/p is 60
+
+#Slicing ### arr[row, column]
+print(arr[:,1]) # Second column
+print(arr[0,:]) # First row
