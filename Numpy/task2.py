6b62763d9007c6766b8997050429747774aef500
sadik <sadikmd2k01@gmail.com>
2025-03-09 13:13:07 +0530

    task 2

---
+++ Numpy/task2.py
@@ -0,0 +1,8 @@
+""" Exercise 1: Create an Array
+Create a NumPy array with these numbers: [5, 10, 15, 20, 25]
+👉 Task: Print the array.
+
+💡 Hint: Use np.array()."""
+import numpy as np
+arr = np.array([5, 10, 15, 20, 25])
+print(arr)
