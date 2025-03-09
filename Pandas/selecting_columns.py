db8ccdb847ee69d171a752259777ad9456316008
sadik <sadikmd2k01@gmail.com>
2025-03-09 14:25:41 +0530

    Select Multiple Columns from data.

---
+++ Pandas/selecting_columns.py
@@ -0,0 +1,12 @@
+import pandas as pd
+data = {
+    "Name": ["Alice", "Bob", "Charlie"],
+    "Age": [25, 30, 35],
+    "City": ["New York", "Los Angeles", "Chicago"]
+}
+df = pd.DataFrame(data)
+print(df["Name"], "\n") # Get only the "Name" column
+
+print(df[["Name", "Age"]], "\n")
+
+print(df[["Name", "Age", "City"]])
