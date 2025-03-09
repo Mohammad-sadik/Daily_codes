d318c0b62b5705c95f3fcd59b80d39802b0756fb
sadik <sadikmd2k01@gmail.com>
2025-03-09 13:17:23 +0530

    task 2

--- Numpy/task2.py
+++ Numpy/task2.py
@@ -6,3 +6,20 @@ Create a NumPy array with these numbers: [5, 10, 15, 20, 25]
 import numpy as np
 arr = np.array([5, 10, 15, 20, 25])
 print(arr)
+"""Exercise 2: Create a 3x3 Array of Zeros
+Make a 3x3 array filled with zeros.
+👉 Task: Print the array.
+
+💡 Hint: Use np.zeros()
+"""
+zeros = np.zeros([3,3],dtype=int)
+print(f"3x3 array filled with zeros: \n {zeros}")
+
+"""🔹 Exercise 3: Pick a Number from an Array
+Given this array:
+
+arr = np.array([10, 20, 30, 40, 50])
+👉 Task: Print the third number in the array.
+
+💡 Hint: Remember, indexing starts from 0!
+"""
