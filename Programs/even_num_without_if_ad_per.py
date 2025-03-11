e5489b75c5927bb2d4ff625a98a9252cfe534e63
sadik <sadikmd2k01@gmail.com>
2025-03-11 15:53:21 +0530

    Without using if condition and % to find even list.

--- Programs/even_num_without_if_ad_per.py
+++ Programs/even_num_without_if_ad_per.py
@@ -1,4 +1,8 @@
-def even(n):
+'''def even(n):
     return 1 & n == 0
 num= int(input("Enter a number: "))
-print(f'the given is even: {even(num)}')
+print(f'the given is even: {even(num)}')'''
+
+even_list = input("Enter numbers : ").split()
+is_even = list(filter(lambda x: (int(x) & 1) == 0, even_list))
+print(f'The even in the list are : {is_even}')
