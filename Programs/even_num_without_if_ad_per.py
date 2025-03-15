620a17512e173d22ae90f0fd59359a7a817c5ff4
sadik <sadikmd2k01@gmail.com>
2025-03-15 15:39:57 +0530

    .

--- Programs/even_num_without_if_ad_per.py
+++ Programs/even_num_without_if_ad_per.py
@@ -3,7 +3,6 @@
 num= int(input("Enter a number: "))
 print(f'the given is even: {even(num)}')'''
 
-
 even_list = input("Enter numbers : ").split()
 is_even = list(filter(lambda x: (int(x) & 1) == 0, even_list))
 print(f'The even in the list are : {is_even}')
