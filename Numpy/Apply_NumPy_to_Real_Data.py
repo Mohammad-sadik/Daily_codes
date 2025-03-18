9502da19938ca1e8ab2a680e328bc64c10efb279
sadik <sadikmd2k01@gmail.com>
2025-03-18 15:25:36 +0530

    .

--- Numpy/Apply_NumPy_to_Real_Data.py
+++ Numpy/Apply_NumPy_to_Real_Data.py
@@ -1,6 +1,5 @@
 
 import numpy as np
-file_path = r"C:\Users\bike_sales.txt"
-data = np.loadtxt(file_path, delimiter=',', skiprows=1)
+file_path = r"C:\users\sadik\Downloads\Data Science\Excel\bike_sales.txt"
+data = np.loadtxt(file_path, delimiter=',', skiprows=1, dtype=str)
 print(data[:5])  # First five rows
-print(np.mean(data, axis=0))  # Column-wise mean
