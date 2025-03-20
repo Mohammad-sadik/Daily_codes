32fc5f5d3c611459abbea1f2a9484b086ad17490
sadik <sadikmd2k01@gmail.com>
2025-03-20 16:14:02 +0530

    .

--- File/copy_one_to_another_file.py
+++ File/copy_one_to_another_file.py
@@ -1,9 +1,11 @@
 f_name = 'copy.txt'
-with open("employee.text", 'r') as file:
-    empl = file.readlines()
+# with open("employee.txt", 'r') as file:
+#     empl = file.readlines()
+# with open(f_name, 'w') as w_file:
+#     w_file.writelines(empl)
+# print('Program Completed....!')
 
-
-with open(f_name, 'w') as w_file:
-    w_file.writelines(empl)
-
+with open('employee.txt', 'r') as r_file, open(f_name, 'w') as w_file:
+    for line in r_file:
+        w_file.writelines(line)
 print('Program Completed....!')
