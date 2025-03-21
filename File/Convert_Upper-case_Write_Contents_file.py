fc78d71e8a367aa35f09f0c25248fac46feb8cd9
sadik <sadikmd2k01@gmail.com>
2025-03-21 08:10:39 +0530

    .

---
+++ File/Convert_Upper-case_Write_Contents_file.py
@@ -0,0 +1,14 @@
+def convert_upper_case():
+    try:
+        with open('copy.txt', 'r') as file:
+            file_data = file.read()
+            file_data = file_data.upper()
+
+        with open('output_file.txt', 'w') as file:
+            file.write(file_data)
+        print(f"\nConversion successful! Uppercase content saved to output_file")
+    except Exception as e:
+        print(f"Error: {e}")
+    pass
+if __name__ == "__main__":
+    convert_upper_case()
