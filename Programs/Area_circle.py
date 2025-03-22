17712e69afb01efccb1d15078e6acc158c392d8f
sadik <sadikmd2k01@gmail.com>
2025-03-22 15:21:43 +0530

    .

--- Programs/Area_circle.py
+++ Programs/Area_circle.py
@@ -1,4 +1,6 @@
 def area_circle(r):
     return 3.14159 * r**2
-radius = float(input("Enter Radius of the circle: "))
-print(f"The radius of the circle is {radius} & the area of the circle is {area_circle(radius):.2f}")
+
+if __name__ =='__main__':
+    radius = float(input("Enter Radius of the circle: "))
+    print(f"The radius of the circle is {radius} & the area of the circle is {area_circle(radius):.2f} Square units.")
