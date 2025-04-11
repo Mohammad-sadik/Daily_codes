ba64fd043747fa2edbb0c35e04031c3bd5d1d86e
sadik <sadikmd2k01@gmail.com>
2025-04-11 22:36:55 +0530

    .

--- Programs using Recursion/Fibonacci_recursive_function.py
+++ Programs using Recursion/Fibonacci_recursive_function.py
@@ -1,8 +1,8 @@
 def fibonacci(n):
     if n <= 1:
-        return n
+        return n # base case
     else:
-        return fibonacci(n-1)+fibonacci(n-2)
+        return fibonacci(n-1)+fibonacci(n-2) # recursive case
 
 num = int(input("Enter the num: "))
 for i in range(num):
